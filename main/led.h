2818a14a491795a8f0e5e02279c5e2e9cb61374e
CrawlToJava <vashkevichigor55@gmail.com>
2023-10-10 22:52:23 +0300

    LED programm has been writenn

---
+++ main/led.h
@@ -0,0 +1,10 @@
+#ifndef LED_MAIN_H
+
+// Configurations
+#define GPIO_NUM_LED 33
+#define DELAY_TIME 1000
+
+void led_app(void);
+
+#define LED_MAIN_H
+#endif
---
+++ main/task_common.h
@@ -0,0 +1,9 @@
+#ifndef TASK_COMMON_H_MAIN_H
+#define TASK_COMMON_H_MAIN_H
+
+// LED task parameters
+#define LED_TASK_PRIORITY 5
+#define LED_TASK_CORE_ID 0
+#define LED_TASK_STACK_SIZE 2048
+
+#endif
